728b217cf252a12ac3530a2cd2188f99734f8171
Your Name <you@example.com>
2023-04-21 08:27:59 -0500

    1

---
+++ 04 Imagen/app.js
@@ -0,0 +1,13 @@
+
+const uno=()=>{
+let img=document.querySelector("#imagen");
+img.src="uno.jpg";
+}
+const dos=()=>{
+let img=document.querySelector("#imagen");
+img.src="dos.jpg";
+}
+const tres=()=> {
+let img = document.querySelector("#imagen");
+img.src ="tres.jpg";
+}
